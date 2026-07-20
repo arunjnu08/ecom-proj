74588412db6ac9fd6eda4f6e7ed81e7f592bd3e2
arunjnu08 <arun.jnumca08@gmail.com>
2026-07-20 21:25:35 +0530

    Code for add entries in DB

---
+++ src/main/java/com/arun/ecom_proj/repo/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.arun.ecom_proj.repo;
+
+import com.arun.ecom_proj.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
